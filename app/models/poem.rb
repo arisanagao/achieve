21f5fba8ad6e01cae7440ba45c203bf06e8cc53a
Arisa NAGAO <a-nagao@Gerhilde.local>
2017-10-10 14:18:07 +0900

    add activeresource

---
+++ app/models/poem.rb
@@ -0,0 +1,5 @@
+class Poem < ActiveResource::Base
+#  self.site = 'https://infinite-sands-45802.herokuapp.com/'
+  self.site = 'https://dive-into-code-arisanagao.c9users.io/'
+#  self.site = 'http://localhost:3000/'
+end
